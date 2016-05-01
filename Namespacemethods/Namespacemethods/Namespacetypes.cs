02827a5e0f5c350cb56914bda8955edbc222ab1d
Zhannna <zhannavardanyan17@mail.ru>
2016-05-01 23:09:25 +0400

    minor

---
+++ Namespacemethods/Namespacemethods/Namespacetypes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace Namespacemethods
+{
+    static class Namespacetypes
+    {
+        public static  Type[] GetTypes(Assembly assembly, string namespac)
+        {
+            return assembly.GetTypes().Where(t => namespac.Equals(t.Namespace)).ToArray();
+
+
+        }
+
+
+        public static MethodInfo[] GetMethods(Assembly assembly, string namespac)
+        {
+            List<MethodInfo> m = new List<MethodInfo>();
+            foreach (Type item in GetTypes(assembly, namespac))
+            {
+                foreach (MethodInfo method in item.GetMethods())
+                {
+                    m.Add(method);
+                    
+                }
+            }
+            return m.ToArray();
+        }
+
+
+        }
+
+
+}
+
---
+++ Namespacemethods/Namespacemethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Namespacemethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Namespacemethods";
+        Type[] t = Namespacetypes.GetTypes(Assembly.GetExecutingAssembly(), str);
+           
+        foreach (Type item in t)
+        {
+            Console.WriteLine(item.Name);
+            
+        }
+
+        foreach (MethodInfo item in Namespacetypes.GetMethods(Assembly.GetExecutingAssembly(), str))
+        {
+            Console.WriteLine(item.Name);
+        }
+            
+        }
+    }
+}
